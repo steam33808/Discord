802bb1e2180cdbe316655d94a041b79ae5db2c84
Alex McKeever <105764282+steam33808@users.noreply.github.com>
2023-03-09 00:20:35 +0000

    random number

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents = intents)
 
-
-
-
-
+@bot.command (name="number")
+async def number(ctx,num):
+  for i in range(int(num)):
+    number1 = random.randint(1,9)
+    ctx.channel.send(number1)
+    await ctx.channel.send(number1)
 
 
 
