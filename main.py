733131167272d283cc5c109a8349d1b93b29c074
Alex McKeever <105764282+steam33808@users.noreply.github.com>
2023-03-14 22:00:14 +0000

    MAde it work

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ bot = commands.Bot(command_prefix="$", intents = intents)
 guess_correct = 0
 
 
-@bot.command(name = "start_game")
-async def start_game(ctx,num):
-
+@bot.command(name = "startgame")
+async def start_game(ctx):
+  
+  def check_for_author(m):
+    return ctx.author == m.author
+  
   global guess_correct
   guess_correct = 0
   number1 = random.randint(1,9)
@@ -22,36 +25,38 @@ async def start_game(ctx,num):
 
   while guess_correct < 4:
     guess_correct=0
-    ctx.send("Please enter your first number")
-    guess1 = int(input(""))
+    await ctx.send("Please enter your first number")
+    guess1 = await bot.wait_for("message", check = check_for_author)#This checks if the number entered was entered by the same person that started the game.
     if guess1 == number1:
-      ctx.send("you got your first number corect")
+      await ctx.send("you got your first number corect")
       guess_correct= guess_correct + 1
     else:
-      ctx.send("Unfortunatly your first number is incorect   please try your second number")
+      await ctx.send("Unfortunatly your first number is incorect   please try your second number")
   
-  
-    guess2 = int(input("Please enter your seccond number"))
+    await ctx.send("Please enter your seccond number")
+    guess2 = await bot.wait_for("message", check = check_for_author)#This checks if the number entered was entered by the same person that started the game.
     if guess2 == number2:
-      ctx.send("you got your first number corect")
+      await ctx.send("you got your forth number corect")
       guess_correct= guess_correct + 1
     else:
-      ctx.send("Unfortunatly your seccond number is incorect please try your third number")
+      await ctx.send("Unfortunatly your seccond number is incorect please try your third number")
   
   
-    guess3 = int(input("Please enter your third number"))
+    await ctx.send("Please enter your third number")
+    guess3 = await bot.wait_for("message", check = check_for_author)#This checks if the number entered was entered by the same person that started the game.
     if guess3 == number3:
-      ctx.send("you got your first number corect")
+      await ctx.send("you got your forth number corect")
       guess_correct= guess_correct + 1
     else:
-      ctx.send("Unfortunatly your third number is incorect please try your third number")
+      await ctx.send("Unfortunatly your seccond number is incorect please try your third number")
   
-    guess4 = int(input("Please enter your forth number"))
+    await ctx.send("Please enter your forth number")
+    guess4 = await bot.wait_for("message", check = check_for_author)#This checks if the number entered was entered by the same person that started the game.
     if guess4 == number4:
-      ctx.send("you got your first number corect")
+      await ctx.send("you got your forth number corect")
       guess_correct= guess_correct + 1
     else:
-      ctx.send("Unfortunatly your forth number is incorect please try again next time")
+      await ctx.send("Unfortunatly your forth number is incorect please try again")
   
   if guess_correct == 4:
     ctx.send("Congrats you guessed corectly")
